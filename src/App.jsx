79f20950d637048092d7c9f24bae2c534f12355a
skippharaoh <caniggiathompson@gmail.com>
2022-03-18 20:56:32 -0400

    TailwindCSS Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="">
+      <h1 className="text-3xl font-bold underline">
         HI! PokéMon FrontEnd Here
       </h1>
     </div>
